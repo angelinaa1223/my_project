a67c6ffa7b4ffd60ed7788a39626c9e15589714a
angelinaa1223 <mitinaagelina128@gmail.com>
2025-12-06 02:05:45 +0200

    My first commit

---
+++ mitina.py/mitina.py
@@ -0,0 +1,37 @@
+def bubble_sort(arr):
+    arr = arr.copy()
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+def selection_sort(arr):
+    arr = arr.copy()
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i + 1, n):
+            if arr[j] < arr[min_index]:
+                min_index = j
+        arr[i], arr[min_index] = arr[min_index], arr[i]
+    return arr
+
+
+def insertion_sort(arr):
+    arr = arr.copy()
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
+
+if __name__ == "__main__":
+    data = [5, 2, 9, 1, 3]
+    print("Bubble:", bubble_sort(data))
+    print("Selection:", selection_sort(data))
+    print("Insertion:", insertion_sort(data))
